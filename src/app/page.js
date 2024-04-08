4b3474490f7735f918a921300df9b26f769b8b29
thucuru <alexjean424@gmail.com>
2024-04-08 00:45:26 -0300

    tela inicial responsiva

--- src/app/page.js
+++ src/app/page.js
@@ -9,12 +9,13 @@ export default function Home() {
         <h1 className="titulo">Consciência Sóbria</h1>
       </header>
       <main>
-        <img src="img1.png" className="ft01"></img>
-        <p>O abuso de drogas e álcool resulta em efeitos imediatos graves e consequências de longo prazo, 
+        <Image src="/img1.png" width={600} height={100} className="ft01" />
+        
+        <p className="descricao">O abuso de drogas e álcool resulta em efeitos imediatos graves e consequências de longo prazo, 
           como dependência física e psicológica, danos ao cérebro e órgãos, problemas financeiros e sociais,
-           além de possíveis consequências legais. Este site oferece informações e recursos para prevenir, 
-           tratar e se recuperar do abuso de substâncias, reconhecendo seu impacto abrangente no indivíduo, 
-           na família, na comunidade e na sociedade.
+          além de possíveis consequências legais. Este site oferece informações e recursos para prevenir, 
+          tratar e se recuperar do abuso de substâncias, reconhecendo seu impacto abrangente no indivíduo, 
+          na família, na comunidade e na sociedade.
         </p>
       </main>
     </div>
