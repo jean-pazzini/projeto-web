0c2c56f80542c0627864a8fba77b7ffe5bc6ec87
thucuru <alexjean424@gmail.com>
2024-04-09 20:39:57 -0300

    atualizando

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,18 @@ export default function Home() {
   return (
     <div className="telaInicial">
       <header>
-        <h1 className="titulo">Consciência Sóbria</h1>
+        {/* Menu dentro do cabeçalho */}
+        <nav className="menu">
+          <ul>
+            <li><a href="gipage.js">Item de Menu 1</a></li>
+            <li><a href="#">Item de Menu 2</a></li>
+            <li><a href="#">Item de Menu 3</a></li>
+          </ul>
+        </nav>
       </header>
+      <div>
+        <h1 className="titulo">Consciência Sóbria</h1>
+      </div>
       <main>
         <Image src="/img1.png" width={600} height={100} className="ft01" />
         
