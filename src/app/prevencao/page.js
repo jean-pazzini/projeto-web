8e4c9b24afcf9fe7a0ceb2ee541dc8ec877e9ad1
Jean Pazzini <jeanpazzinidomingues@gmail.com>
2024-04-11 14:24:27 -0300

    corrigindo pequenos bugs e melhorando identação

--- src/app/prevencao/page.js
+++ src/app/prevencao/page.js
@@ -22,28 +22,27 @@ export default function Home() {
 
         <div className="descricao-container">
           <h3>Prevenção</h3>
-          <h4>
-            Por que prevenção é importante ?
-          </h4>
+          <h4>Por que prevenção é importante ?</h4>
           <p>
-
-            A prevenção do abuso de substâncias, que inclui o abuso de drogas entorpecentes e o uso nocivo do álcool, é fundamental para mitigar os danos individuais, sociais e de saúde pública associados a essas práticas. Aqui estão algumas abordagens importantes para prevenir o abuso de substâncias:
+            A prevenção do abuso de substâncias, que inclui o abuso de drogas entorpecentes e o 
+            uso nocivo do álcool, é fundamental para mitigar os danos individuais, sociais e de 
+            saúde pública associados a essas práticas. Aqui estão algumas abordagens importantes para 
+            prevenir o abuso de substâncias:
           </p>
         </div>
 
-
-          <div className="video">
-            <iframe 
-              width="560" 
-              height="315" 
-              src="https://www.youtube.com/embed/7CEJkPCc7z4" 
-              title="YouTube video player" 
-              frameborder="0" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-              allowfullscreen
-              style={{boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', borderRadius: '8px'}}
-            ></iframe>
-          </div>
+        <div className="video">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/7CEJkPCc7z4" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen
+            style={{boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', borderRadius: '8px'}}>
+          </iframe>
+        </div>
 
         <div className="opcoes-container">
           <h4>O que você deve saber ?</h4>
@@ -51,14 +50,21 @@ export default function Home() {
             <li>
               <h4>Educação e conscientização: </h4>
               <ul>
-                <li>A educação sobre os efeitos nocivos das substâncias e os riscos associados ao seu uso é essencial. Isso inclui informações sobre os efeitos de curto e longo prazo das substâncias, os riscos à saúde física e mental, os danos sociais e legais e os recursos disponíveis para tratamento e recuperação.</li>
+                <li>A educação sobre os efeitos nocivos das substâncias e os riscos associados ao 
+                  seu uso é essencial. Isso inclui informações sobre os efeitos de curto e longo prazo 
+                  das substâncias, os riscos à saúde física e mental, os danos sociais e legais e os 
+                  recursos disponíveis para tratamento e recuperação.</li>
               </ul>
             </li>
 
             <li>
               <h4>Desenvolvimento de habilidades pessoais e sociais:</h4>
               <ul>
-                <li>Fortalecer habilidades de resistência à pressão dos pares, habilidades de resolução de problemas, autoestima e habilidades de comunicação pode ajudar os indivíduos a evitar o abuso de substâncias. Programas de desenvolvimento de habilidades sociais e emocionais podem ser implementados em escolas, locais de trabalho e comunidades para promover o bem-estar e a resiliência.
+                <li>Fortalecer habilidades de resistência à pressão dos pares, habilidades de resolução 
+                  de problemas, autoestima e habilidades de comunicação pode ajudar os indivíduos a 
+                  evitar o abuso de substâncias. Programas de desenvolvimento de habilidades sociais e 
+                  emocionais podem ser implementados em escolas, locais de trabalho e comunidades para 
+                  promover o bem-estar e a resiliência.
                 </li>
               </ul>
             </li>
@@ -66,7 +72,11 @@ export default function Home() {
             <li>
               <h4>Ambientes saudáveis e suporte comunitário:</h4>
               <ul>
-                <li>Criar ambientes que promovam estilos de vida saudáveis e oferecer apoio social e comunitário pode ajudar a prevenir o abuso de substâncias. Isso pode incluir a implementação de políticas de controle de substâncias em locais públicos, a disponibilidade de serviços de aconselhamento e suporte, e a promoção de atividades recreativas e de lazer saudáveis.
+                <li>Criar ambientes que promovam estilos de vida saudáveis e oferecer apoio social e 
+                  comunitário pode ajudar a prevenir o abuso de substâncias. Isso pode incluir a 
+                  implementação de políticas de controle de substâncias em locais públicos, a 
+                  disponibilidade de serviços de aconselhamento e suporte, e a promoção de atividades 
+                  recreativas e de lazer saudáveis.
                 </li>
               </ul>
             </li>
@@ -74,21 +84,34 @@ export default function Home() {
             <li>
               <h4>Intervenção precoce e identificação de riscos:</h4>
               <ul>
-                <li>Identificar e intervir precocemente em comportamentos de risco relacionados ao abuso de substâncias pode ajudar a prevenir problemas mais graves no futuro. Isso pode ser feito por meio de triagem de saúde mental, programas de prevenção nas escolas, e acesso a serviços de aconselhamento e tratamento.</li>
+                <li>Identificar e intervir precocemente em comportamentos de risco relacionados ao abuso 
+                  de substâncias pode ajudar a prevenir problemas mais graves no futuro. Isso pode ser 
+                  feito por meio de triagem de saúde mental, programas de prevenção nas escolas, e acesso 
+                  a serviços de aconselhamento e tratamento.
+                </li>
               </ul>
             </li>
 
             <li>
               <h4>Tratamento e recuperação acessíveis:</h4>
               <ul>
-                <li>Garantir o acesso a serviços de tratamento e recuperação de qualidade para aqueles que lutam contra o abuso de substâncias é fundamental. Isso inclui o acesso a serviços de desintoxicação, aconselhamento individual e em grupo, terapia medicamentosa, e apoio contínuo para prevenir recaídas.</li>
+                <li>Garantir o acesso a serviços de tratamento e recuperação de qualidade para aqueles que 
+                  lutam contra o abuso de substâncias é fundamental. Isso inclui o acesso a serviços de 
+                  desintoxicação, aconselhamento individual e em grupo, terapia medicamentosa, e apoio 
+                  contínuo para prevenir recaídas.
+                </li>
               </ul>
             </li>
 
             <li>
               <h4>Políticas públicas eficazes: </h4>
               <ul>
-                <li>A implementação de políticas públicas voltadas para a redução da disponibilidade, acessibilidade e aceitabilidade social das substâncias pode desempenhar um papel importante na prevenção do abuso de substâncias. Isso pode incluir restrições à venda de substâncias psicoativas, regulamentação da publicidade de produtos relacionados a drogas e investimento em programas de tratamento e reabilitação.</li>
+                <li>A implementação de políticas públicas voltadas para a redução da disponibilidade, 
+                  acessibilidade e aceitabilidade social das substâncias pode desempenhar um papel 
+                  importante na prevenção do abuso de substâncias. Isso pode incluir restrições à venda 
+                  de substâncias psicoativas, regulamentação da publicidade de produtos relacionados a 
+                  drogas e investimento em programas de tratamento e reabilitação.
+                </li>
               </ul>
             </li>
           </ol>
--- src/app/recursos/page.js
+++ src/app/recursos/page.js
@@ -14,20 +14,20 @@ export default function Home() {
           <ul>
             <li><Link href="../prevencao">Prevenção</Link></li>
             <li><Link href="../tratamento">Tratamento</Link></li>
-            <li><Link href="../">Recursos</Link></li>
+            <li><Link href="../recursos">Recursos</Link></li>
           </ul>
         </nav>
       </header>
       <main className="conteudo">
-
         <div className="descricao-container">
           <h3>Recursos</h3>
           <h4>
             Por que prevenção é importante ?
           </h4>
           <p>
-          
-Quando se trata de lidar com o abuso de substâncias, incluindo drogas entorpecentes e o uso nocivo do álcool, é fundamental ter acesso a uma variedade de recursos confiáveis que possam oferecer suporte, orientação e assistência. Aqui estão alguns recursos possíveis:
+            Quando se trata de lidar com o abuso de substâncias, incluindo drogas entorpecentes 
+            e o uso nocivo do álcool, é fundamental ter acesso a uma variedade de recursos confiáveis 
+            que possam oferecer suporte, orientação e assistência. Aqui estão alguns recursos possíveis:
           </p>
         </div>
         <div className="opcoes-container">
@@ -35,14 +35,22 @@ Quando se trata de lidar com o abuso de substâncias, incluindo drogas entorpece
             <li>
               <h4>Centros de Atenção Psicossocial (CAPS): </h4>
               <ul>
-                <li>Os CAPS oferecem assistência especializada em saúde mental, incluindo tratamento para dependência química. Eles fornecem avaliação, suporte psicológico, terapia em grupo e encaminhamento para outros serviços quando necessário.</li>
+                <li>
+                  Os CAPS oferecem assistência especializada em saúde mental, incluindo tratamento 
+                  para dependência química. Eles fornecem avaliação, suporte psicológico, terapia 
+                  em grupo e encaminhamento para outros serviços quando necessário.
+                </li>
               </ul>
             </li>
 
             <li>
               <h4>Centros de Acolhimento e Reabilitação:</h4>
               <ul>
-                <li>Existem várias instituições, públicas e privadas, dedicadas à reabilitação de dependentes químicos. Esses centros oferecem programas de desintoxicação, acompanhamento médico, terapia individual e em grupo, além de atividades de reintegração social.
+                <li>
+                  Existem várias instituições, públicas e privadas, dedicadas à reabilitação 
+                  de dependentes químicos. Esses centros oferecem programas de desintoxicação, 
+                  acompanhamento médico, terapia individual e em grupo, além de atividades de 
+                  reintegração social.
                 </li>
               </ul>
             </li>
@@ -50,7 +58,11 @@ Quando se trata de lidar com o abuso de substâncias, incluindo drogas entorpece
             <li>
               <h4>Programas de Prevenção e Educação:</h4>
               <ul>
-                <li>O governo e organizações não governamentais (ONGs) realizam programas de prevenção e educação sobre drogas e álcool em escolas, comunidades e locais de trabalho. Esses programas visam conscientizar sobre os riscos do abuso de substâncias e promover estilos de vida saudáveis.
+                <li>
+                  O governo e organizações não governamentais (ONGs) realizam programas 
+                  de prevenção e educação sobre drogas e álcool em escolas, comunidades 
+                  e locais de trabalho. Esses programas visam conscientizar sobre os riscos do abuso 
+                  de substâncias e promover estilos de vida saudáveis.
                 </li>
               </ul>
             </li>
@@ -58,7 +70,12 @@ Quando se trata de lidar com o abuso de substâncias, incluindo drogas entorpece
             <li>
               <h4>Linhas de Ajuda e Serviços de Apoio Telefônico:</h4>
               <ul>
-                <li>Há linhas telefônicas de ajuda e serviços de apoio disponíveis para aqueles que precisam de assistência imediata ou desejam obter informações sobre tratamento e suporte. Esses serviços são confidenciais e oferecem orientação de profissionais treinados.</li>
+                <li>
+                  Há linhas telefônicas de ajuda e serviços de apoio disponíveis para aqueles que 
+                  precisam de assistência imediata ou desejam obter informações sobre tratamento e 
+                  suporte. Esses serviços são confidenciais e oferecem orientação de profissionais 
+                  treinados.
+                </li>
               </ul>
             </li>
           </ol>
