5ede87643f76ed6be4d8706e89bc2bda22239b50
thucuru <alexjean424@gmail.com>
2024-04-10 21:23:32 -0300

    Projeto Atualizado

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,14 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="telaInicial">
+      <>
       <header>
-        {/* Menu dentro do cabeçalho */}
         <nav className="menu">
+        <img src="/image/logo.jpg" className="logo"></img>
           <ul>
-            <li><a href="gipage.js">Item de Menu 1</a></li>
-            <li><a href="#">Item de Menu 2</a></li>
-            <li><a href="#">Item de Menu 3</a></li>
+            <li><a href="gipage.js">Prevenção</a></li>
+            <li><a href="#">Tratamento</a></li>
+            <li><a href="#">Recursos</a></li>
           </ul>
         </nav>
       </header>
@@ -19,15 +19,18 @@ export default function Home() {
         <h1 className="titulo">Consciência Sóbria</h1>
       </div>
       <main>
-        <Image src="/img1.png" width={600} height={100} className="ft01" />
-        
-        <p className="descricao">O abuso de drogas e álcool resulta em efeitos imediatos graves e consequências de longo prazo, 
+        <Image src="/image/img1.png" width={600} height={100} className="ft01" />
+
+        <p className="descricao">O abuso de drogas e álcool resulta em efeitos imediatos graves e consequências de longo prazo,
           como dependência física e psicológica, danos ao cérebro e órgãos, problemas financeiros e sociais,
-          além de possíveis consequências legais. Este site oferece informações e recursos para prevenir, 
-          tratar e se recuperar do abuso de substâncias, reconhecendo seu impacto abrangente no indivíduo, 
+          além de possíveis consequências legais. Este site oferece informações e recursos para prevenir,
+          tratar e se recuperar do abuso de substâncias, reconhecendo seu impacto abrangente no indivíduo,
           na família, na comunidade e na sociedade.
         </p>
+        <footer class="footer">
+          <p>Consciência Sóbria</p>
+        </footer> 
       </main>
-    </div>
+      </>
   );
 }
